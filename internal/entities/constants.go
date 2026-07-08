d3ef616965b34a0f04462a47cf9dde5fd722aec5
Bhupesh-V <varshneybhupesh@gmail.com>
2026-07-08 16:45:21 +0530

    feat: add implementation of set/get tokens

--- internal/entities/constants.go
+++ internal/entities/constants.go
@@ -2,9 +2,12 @@ package entities
 
 // Names of all AFK secret "keys" being stored in user's system keyring (other than workspace tokens)
 const (
-	KEYRING_APP_NAME                string = "afk"
-	KEYRING_AFK_SLACK_CLIENT_ID     string = "afk_slack_app_client_id"
-	KEYRING_AFK_SLACK_CLIENT_SECRET string = "afk_slack_app_client_secret"
+	KEYRING_APP_NAME            string = "afk"
+	KEYRING_SLACK_CLIENT_ID     string = "afk_slack_app_client_id"
+	KEYRING_SLACK_CLIENT_SECRET string = "afk_slack_app_client_secret"
+	KEYRING_SLACK_TEAM_IDS      string = "afk_slack_team_ids"
+	KEYRING_TEAM_SECRET_PREFIX  string = "afk_slack_team_secret"
+	KEYRING_SECRET_SEP          string = "|"
 )
 
 // AFK config file constants
---
+++ internal/entities/slack.go
@@ -0,0 +1,6 @@
+package entities
+
+type SlackMetaData struct {
+	TeamName       string
+	WorkspaceToken string
+}
